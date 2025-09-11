5e0449addab5f610e98e0087aacd8630cd8964b0
Syifa <Syifa>
2025-09-11 14:41:40 +0700

    feat: update database

--- config/database.php
+++ config/database.php
@@ -89,7 +89,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD_FILE', ''),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
